ac83f9bd89f2689b8df1e3359204d104a9ee4d70
samuelva <samuelvanapeldoorn@outlook.com>
2016-02-10 20:00:22 +0100

    Cleaned the code a bit.

--- RedditWallpaper.py
+++ RedditWallpaper.py
@@ -19,14 +19,15 @@ def main(subredditChoice):
 
     for submission in subreddit.get_top_from_day():
         image_name = submission.url.split("/")[-1]
+        
         print(submission.url)
-        if not validate_submission(image_name):
+
+        if not allowed_extension(image_name):
             continue
 
         urllib.request.urlretrieve(submission.url, directory+image_name)
-        resolution = get_image_size(directory+image_name)
 
-        if validate_resolution(resolution):
+        if allowed_resolution(directory+image_name):
             os.system("gsettings set org.gnome.desktop.background picture-uri file://%(path)s" % {'path':directory+image_name})
             os.system("gsettings set org.gnome.desktop.background picture-options wallpaper")
             break
@@ -34,60 +35,20 @@ def main(subredditChoice):
             os.system("rm %s" % directory+image_name)
 
 
-def validate_submission(image_name):
-    print(image_name.split(".")[-1])
-    if image_name.split(".")[-1] in ["jpg", "png"]:
+def allowed_extension(image):
+    if image.split(".")[-1] in ["jpg", "png"]:
         return True
     else:
         False
 
-def get_image_size(image):
-    with Image.open(image) as im:
-        print("grootte", im.size)
-    return im.size
-
-# def get_image_size(fname):
-#     with open(fname, 'rb') as fhandle:
-#         head = fhandle.read(24)
-#         if len(head) != 24:
-#             return
-#         if imghdr.what(fname) == 'png':
-#             check = struct.unpack('>i', head[4:8])[0]
-#             if check != 0x0d0a1a0a:
-#                 return
-#             width, height = struct.unpack('>ii', head[16:24])
-#         elif imghdr.what(fname) == 'gif':
-#             width, height = struct.unpack('<HH', head[6:10])
-#         elif imghdr.what(fname) == 'jpeg':
-#             try:
-#                 fhandle.seek(0)
-#                 size = 2
-#                 ftype = 0
-#                 while not 0xc0 <= ftype <= 0xcf:
-#                     fhandle.seek(size, 1)
-#                     byte = fhandle.read(1)
-#                     while ord(byte) == 0xff:
-#                         byte = fhandle.read(1)
-#                     ftype = ord(byte)
-#                     size = struct.unpack('>H', fhandle.read(2))[0] - 2
-#                 fhandle.seek(1, 1)
-#                 height, width = struct.unpack('>HH', fhandle.read(4))
-#             except Exception:
-#                 return
-#         else:
-#             return
-#         return width, height
-
-def validate_resolution(resolution):
-    if resolution in ["", " "]:
-        return False
-    if resolution[0] >= 1920 and resolution[1] >= 1080:
-        return True
-    else:
-        return False
+def allowed_resolution(image):
+    with Image.open(image) as img:
+        if img.size[0] >= 1920 and img.size[1] >= 1080:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
-    print(len(sys.argv))
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
